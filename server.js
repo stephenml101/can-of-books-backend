d7b8013ba20ae980d0ceed3302ddfbbccec00339
Stephen <stephenlevesque2@gmail.com>
2023-03-29 14:50:33 -0700

    update Form

--- server.js
+++ server.js
@@ -87,18 +87,11 @@ async function postBook(request,response,next){
   }
 }
 
-app.get('*', (request, response) => {
-  response.statusMessage(404).send('Not available');
-});
-
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-});
 
 // *** Endpoint to update a book ***
 app.put('/books/:bookID', updateBook);
 
-async function updateCat(request, response, next) {
+async function updateBook(request, response, next) {
   try {
     // ID - the book to update, DATA - the info to update the book with
     let id = request.params.bookID;
@@ -111,9 +104,10 @@ async function updateCat(request, response, next) {
     response.status(200).send(updatedBook);
    
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
+
 
 app.get('*', (request, response) => {
   response.statusMessage(404).send('Not available');
@@ -122,3 +116,4 @@ app.get('*', (request, response) => {
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
+
